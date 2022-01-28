ba45b9a7b6a56b1270474e1d05e7b98f6f51eeef
Kenneth Brandon <kbrandon319@gmail.com>
2022-01-28 17:21:20 -0500

    Completion of  slider progress effect

--- js/app.js
+++ js/app.js
@@ -5,9 +5,7 @@ let output = document.getElementById('price');
 
 
 
-//outputting the slider value
-
-//output.textContent = '$16.00';
+//outputting the slider value so when the page loads the slider is on 16 by default
 output.textContent = "$" + slider.value + ".00";
 
 slider.oninput =  function(){
@@ -21,9 +19,12 @@ slider.oninput =  function(){
 //filling the range with color when thumb is moved
 slider.addEventListener('input',function(){
    
-    
-    var x = ((slider.value - slider.min) / (slider.max - slider.min) * 100 ); 
-    let color = `linear-gradient(90deg, rgb(165, 243, 235) ${x}%, rgb(165, 243, 235) ${x}%)`;
+    let x = Number(slider.value); 
+    let max = Number(slider.max);
+    let changeValue = x/max * 100; 
+
+    //let color = `linear-gradient(90deg, rgb(165, 243, 235) ${x}%, rgb(165, 243, 235) ${x}%)`;
+    let color = `linear-gradient(90deg, hsl(174, 76%, 80%) 0% ${changeValue}%, rgb(234, 238, 251) ${changeValue}% 100%)`;
 
 
     slider.style.background = color;
