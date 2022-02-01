31ea9562cf2d4f85d316648e76edd4c12b66e1f1
Kenneth Brandon <kbrandon319@gmail.com>
2022-02-01 16:35:18 -0500

    Working on having the page view numbers change in relation to the slider position

--- js/app.js
+++ js/app.js
@@ -1,16 +1,57 @@
 let slider = document.getElementById('slider');
 let output = document.getElementById('price');
+let pageView = document.getElementById('pageview');
 
+let defaultPageView = " ";
+/*
+Here are the different page view ranges and the corresponding monthly price totals:
 
+- 10K pageviews / $8 per month
+- 50K pageviews / $12 per month
+- 100K pageviews / $16 per month
+- 500k pageviews / $24 per month
+- 1M pageviews / $36 per month
 
+If the visitor switches the toggle to yearly billing, a 25% discount should be applied to all prices.
+*/
 
 
 //outputting the slider value so when the page loads the slider is on 16 by default
+pageView.textContent = defaultPageView;
 output.textContent = "$" + slider.value + ".00";
 
+//allows the number to correspond to the position of the slide
 slider.oninput =  function(){
 
     output.textContent = '$'+ slider.value +'.00';
+
+    let tenK = '10K pageviews';
+    let fiftyK = '50K pageviews';
+    let hundredK = '100K pageviews';
+    let fivehundredK = '500K pageviews';
+    let oneMil = '1M pageviews';
+
+    if(slider.value == 8){
+        pageView.textContent = tenK;
+    }
+    else if(slider.value == 12){
+        pageView.textContent = fiftyK;
+    }
+    
+    else if(slider.value == 16){
+        pageView.textContent = hundredK;
+    }
+    else if(slider.value == 24){
+        pageView.textContent = fivehundredK;
+    }
+    else if(slider.value == 36){
+        pageView.textContent = oneMil;
+    }
+    else{
+        pageView.textContent = defaultPageView;
+    }
+
+    
 
 }
 
